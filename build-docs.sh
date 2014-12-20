d40076f47844a4d1d4d42998eebcc8738cd4b1e3
Mark Watts <wattsmark2015@gmail.com>
2014-12-19 21:10:10 -0600

    Moved Java API documentation to its own location

--- build-docs.sh
+++ build-docs.sh
@@ -1,1 +1,2 @@
-javadoc -sourcepath src/main/java -d docs.build @docs/javadoc-packages @docs/javadoc-args
+javadoc -sourcepath src/main/java -d api-docs.build @java-docs/javadoc-packages @java-docs/javadoc-args
+sphinx-build -w sphinx-errors docs prose-docs.build
